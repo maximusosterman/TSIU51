6c614f1e17c07adf65dc0864e644662616035490
Maximus Vetterholm Österman <maxve266@student.liu.se>
2025-01-29 17:55:27 +0100

    TWI_81 - sending data, both adress and data

--- lab1/lab1/main.asm
+++ lab1/lab1/main.asm
@@ -13,6 +13,44 @@
 	.equ SCL = PC0
 	.equ SDA = PC1
 
+MAIN:
+	ldi		r16, HIGH(RAMEND)
+	out		SPH, r16
+	ldi		r16, LOW(RAMEND)
+	out		SPL, r16
+
+TWI_SEND: // ALWAYS SEND r20
+	call	START
+	call	ADRESS
+	call	DATA
+	call	STOP
+
+ADRESS:
+	ldi		r20, ADDR_LEFT8*2
+	call	TWI_81
+	ret
+
+DATA:
+	mov		r20, r17
+	call	TWI_81
+	ret
+
+
+TWI_81:
+	ldi		r18,8 // loop counter	
+LOOP:
+	sbrc	r20, 7
+	call	SDH
+	sbrs	r20, 7
+	call	SDL
+	lsl		r20
+	dec		r18
+	cpi		r18, 0
+	brne	LOOP // end of loop
+
+	call	SDH	//ACK 
+	ret
+
 START:
 	sbi		DDRC, SDA
 	call	WAIT
@@ -41,8 +79,12 @@ SDL:
 SDH:
 	cbi		DDRC, SDA
 	call	WAIT
-	cbi		DDRC, SDL
+	cbi		DDRC, SCL
 	call	WAIT
 	sbi		DDRC, SCL
 	call	WAIT
+	ret
+
+WAIT:
+
 	ret
