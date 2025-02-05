b216d484db7375675150a501e041276a4598218a
Maximus Vetterholm Österman <maxve266@student.liu.se>
2025-02-05 19:54:31 +0100

    working lab2

--- lab2/lab2/main.asm
+++ lab2/lab2/main.asm
@@ -5,7 +5,9 @@
 ; Author : maxve266
 ;
 ;
+	.equ ADDR_LEFT8 = $24
 	.equ ADDR_JOY =	$27
+	.equ ADDR_LEDS = $26 
 	.equ SCL = PC0
 	.equ SDA = PC1
 
@@ -19,7 +21,108 @@ MAIN:
 	ldi		r20, ADDR_JOY 
 	call	SET_READ_ADDR	
 	call	TWI_SEND
+	call	PRINT_NUMBER	
 	rjmp	MAIN
+
+SET_SIX:
+	ldi		r20, ADDR_LEFT8*2
+	call	START
+	call	TWI_81 
+	ldi		r20, 0b1111101
+	call	TWI_81
+	call	STOP
+	rjmp	CONTINUE
+
+SET_FIVE:
+	ldi		r20, ADDR_LEFT8*2
+	call	START
+	call	TWI_81 
+	ldi		r20, 0b1101101
+	call	TWI_81
+	call	STOP 
+	rjmp	CONTINUE
+
+PRINT_NUMBER:
+	andi	r20, $3F
+	cpi		r20, $3F  
+	breq	SET_ZERO
+	cpi		r20, $3E
+	breq	SET_ONE
+	cpi		r20, $3D
+	breq	SET_TWO 
+	cpi		r20, $3B
+	breq	SET_THREE
+	cpi		r20, $37
+	breq	SET_FOUR 
+	cpi		r20, $2F
+	breq	SET_FIVE
+	cpi		r20, $1F
+	breq	SET_SIX
+
+SET_ZERO:
+	ldi		r20, ADDR_LEFT8*2
+	call	START
+	call	TWI_81 
+	ldi		r20, 0b0111111
+	call	TWI_81
+	call	STOP
+	rjmp	CONTINUE
+
+SET_ONE:
+	ldi		r20, ADDR_LEFT8*2
+	call	START
+	call	TWI_81 
+	ldi		r20, 0b0000110
+	call	TWI_81
+	call	STOP
+	rjmp	CONTINUE
+
+SET_TWO:
+	ldi		r20, ADDR_LEFT8*2
+	call	START
+	call	TWI_81 
+	ldi		r20, 0b1011011
+	call	TWI_81	
+	call	STOP
+	rjmp	CONTINUE
+
+SET_THREE:
+	ldi		r20, ADDR_LEFT8*2
+	call	START
+	call	TWI_81 
+	ldi		r20, 0b1001111
+	call	TWI_81
+	call	STOP 
+	call	START
+	ldi		r17, 2
+	call	MAIN_CONT
+	call	STOP
+	rjmp	CONTINUE
+
+SET_FOUR:
+	ldi		r20, ADDR_LEFT8*2
+	call	START
+	call	TWI_81 
+	ldi		r20, 0b1100110
+	call	TWI_81
+	call	STOP
+	call	START
+	ldi		r17, 1
+	call	MAIN_CONT
+	call	STOP
+	rjmp	CONTINUE
+
+
+CONTINUE:
+	ret
+
+
+MAIN_CONT:
+	ldi		r20, ADDR_LEDS*2
+	call	TWI_81
+	mov		r20, r17
+	call	TWI_81 
+	ret
 
 SET_READ_ADDR:
 	lsl		r20
@@ -39,12 +142,13 @@ READ_TWI:
 	ldi		r18, 8 // loop counter	
 LOOP_READ:
 	cbi		DDRC, SCL
+	call	WAIT
 	lsl		r20
 	sbic	PINC, SDA
 	ori		r20, 1
-	dec		r18
 	sbi		DDRC, SCL
-	cpi		r18, 0
+	call	WAIT
+	dec		r18
 	brne	LOOP_READ // end of loop
 	call	SDH	//ACK 
 	ret	
@@ -106,4 +210,3 @@ LOOP:
 
 	call	SDH	//ACK 
 	ret
-
