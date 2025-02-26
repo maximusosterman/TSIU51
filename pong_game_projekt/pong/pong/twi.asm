e02ed1301bcc8a7af2aa49a2447c3733ea4f4f4a
Maximus Wetterholm-Österman <maximus@oesterman.com>
2025-02-26 22:01:15 +0100

    added twi boillerplate code

---
+++ pong_game_projekt/pong/pong/twi.asm
@@ -0,0 +1,82 @@
+#ifndef _TWI_
+#define _TWI_
+
+TWI_81:
+	ldi		r18,8 // loop counter
+LOOP:
+	sbrc	r20, 7
+	call	SDH
+	sbrs	r20, 7
+	call	SDL
+	lsl		r20
+	dec		r18
+	cpi		r18, 0
+	brne	LOOP // end of loop
+
+	call	SDH	//ACK
+	ret
+
+TWI_START:
+	sbi		DDRC, SDA
+	call	WAIT_TWI
+	sbi		DDRC, SCL
+	call	WAIT_TWI
+	ret
+
+TWI_STOP:
+	sbi		DDRC, SDA
+	call	WAIT_TWI
+	cbi		DDRC, SCL
+	call	WAIT_TWI
+	cbi		DDRC, SDA
+	call	WAIT_TWI
+	ret
+
+SDL:
+	sbi		DDRC, SDA
+	call	WAIT_TWI
+	cbi		DDRC, SCL
+	call	WAIT_TWI
+	sbi		DDRC, SCL
+	call	WAIT_TWI
+	ret
+
+SDH:
+	cbi		DDRC, SDA
+	call	WAIT_TWI
+	cbi		DDRC, SCL
+	call	WAIT_TWI
+	sbi		DDRC, SCL
+	call	WAIT_TWI
+	ret
+
+WAIT_TWI:
+		push	r16
+		ldi		r16,10 ; Decimal bas
+WAIT_inner_loop:
+		dec		r16
+		brne	WAIT_inner_loop
+		pop		r16
+		ret
+
+delay_1sec:
+		push	r16
+		push	r17
+		push	r18
+		ldi		r18, $FF
+		ldi		r16,10 ; Decimal bas
+delay1sec_outer_loop:
+		ldi		r17,$1F
+delay1sec_inner_loop:
+		dec		r17
+		brne	delay1sec_inner_loop
+		dec		r16
+		brne	delay1sec_outer_loop
+		dec		r18
+		brne	delay1sec_outer_loop
+		pop		r18
+		pop		r17
+		pop		r16
+		ret
+
+#ednif /*_TWI*/
