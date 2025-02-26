f7f65102f84f892ccea397070da08d4a5f7ec0f1
Max Ringbäck <maxri670@student.liu.se>
2025-02-26 22:09:42 +0100

    added startpos for player and created labels for listen functions

--- pong_game_projekt/pong/pong/game.asm
+++ pong_game_projekt/pong/pong/game.asm
@@ -3,27 +3,52 @@
 #define _GAME_
 
 .dseg
-	PLAYER: .byte 1
+	PLAYER_1: .byte 1
+	PLAYER_2: .byte 1 
 .cseg
 
 GAME_LOOP:
 	
 	call	ERASE_VMEM 
+	
 	call	GET_PLAYER_1_POS
 	call	GET_PLAYER_2_POS
 	
 
-ret
-
-
+	ret 
 
 GET_PLAYER_1_POS:
-
-
-
-
-
+		
+	call	GET_STARTER_1_POS
+	call	LISTEN_TO_RIGHT_JOYSTICK
+	ret
 
 GET_PLAYER_2_POS:
+	
+	call	GET_STARTER_2_POS 
+	call	LISTEN_TO_LEFT_JOYSTICK
+
+	ret
+
+GET_PLAYER_1_POS:
+	
+	ldi		r16, $04
+	sts		PLAYER_1, r16
+	ret
+
+
+GET_STARTER_2_POS
+
+	ldi		r16, $F4 
+	sts		PLAYER_2, r16 
+	ret 
+
+
+
+
+GET_STARTER_2_POS
+
+
+	ret 
 
 #endif /* _game_ */
