7c7473e6e25c5412d6ba972b1b594b2f3b5eee1e
Maximus Wetterholm-Österman <maximus@oesterman.com>
2025-02-26 18:57:07 +0100

    added new replaced reverse func

--- pong_game_projekt/pong/pong/helpers.asm
+++ pong_game_projekt/pong/pong/helpers.asm
@@ -1,21 +1,18 @@
 #ifndef _HELPERS_
 #define _HELPERS_
 
-REVERSE_REGISTER: // REVERSERS r16
-	push	r17
-	push	r18
+REVERSE_REGISTER_LUT:
+    push ZH
+    push ZL
 
-	ldi		r17, 8
-	clr		r18
+    ldi ZH, high(REVERSE_LUT<<1)  ; Load LUT address
+    ldi ZL, low(REVERSE_LUT<<1)
+    add ZL, r16                   ; Use r16 as index
+    adc ZH, r1                    ; Add carry if needed
+    lpm r16, Z                    ; Load reversed byte into r16
 
-REVERSE_LOOP:
-	lsr		r16  // Shift left 
-	rol		r16	//Rotate left, Carry goes to LSB
-	dec		r17
-	brne	REVERSE_LOOP
-	
-	pop		r18
-	pop		r17
-	ret
+    pop ZL
+    pop ZH
+    ret
 
 #endif /* _HELPERS_ */
