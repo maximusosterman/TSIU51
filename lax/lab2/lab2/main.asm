6972d1800d730e30a40fc26bb8a255ec0ae74754
Maximus Wetterholm-Österman <maxve266@student.liu.se>
2025-05-29 11:50:24 +0200

    added lab2 done

--- lax/lab2/lab2/main.asm
+++ lax/lab2/lab2/main.asm
@@ -5,8 +5,52 @@
 ; Author : maxve266
 ;
 
+.def num_of_clicks = r17
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+COLD:
+	ldi r16, HIGH(RAMEND)
+	out SPH, r16
+	ldi r16, LOW(RAMEND)
+	out SPL, r16
+	call HW_INIT
+	clr num_of_clicks
+	jmp MAIN
+
+MAIN:
+	call READ
+	jmp DISPLAY
+
+READ:
+	sbic PIND, 0
+	call WAIT
+	ret
+
+WAIT:
+	sbic PIND, 0
+	jmp WAIT
+	cpi	num_of_clicks, 15
+	breq DISPLAY
+	inc num_of_clicks
+	ret
+
+DISPLAY:
+	sbic PIND, 1 
+	jmp DISPLAY_MODE
+	jmp MAIN
+
+DISPLAY_MODE:
+	out PORTB, num_of_clicks
+	sbis PIND, 0
+	jmp DISPLAY_MODE
+	clr num_of_clicks
+	jmp MAIN
+	 
+	
+
+
+HW_INIT:
+	ldi r16, $FF
+	out DDRB, r16
+	ldi r16, $0
+	out DDRD, r16
+	ret
